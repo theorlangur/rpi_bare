2c028f1d2898ef6498897968effbe930cac33155
theorlangur <theorlangur@gmail.com>
2022-12-26 22:22:11 +0100

    removed restriction for bits count from uint8_t to any convertible

--- src/rpi_bare/rpi_bare.h
+++ src/rpi_bare/rpi_bare.h
@@ -23,39 +23,39 @@ namespace rpi
         template<class L>
         auto ScopeExit(L l){return ExitScopeObj<L>(static_cast<L&&>(l));}
 
-        template<uint8_t N>
+        template<auto N>
         constexpr uint32_t clear_bits(uint32_t from, uint32_t v)
         {
-            const uint32_t kClearMask = ((uint32_t(1) << N) - 1) << uint32_t(from);
+            const uint32_t kClearMask = ((uint32_t(1) << uint8_t(N)) - 1) << uint32_t(from);
             return (v & ~kClearMask);
         }
 
-        template<uint8_t N, class E>
+        template<auto N, class E>
         constexpr uint32_t set_bits(uint32_t from, uint32_t v, E bits)
         {
             return clear_bits<N>(from, v) | (uint32_t(bits) << uint32_t(from));
         }
 
-        template<uint8_t N, class E>
+        template<auto N, class E>
         constexpr void set_bits(volatile uint32_t *pFrom, uint32_t v, E bits)
         {
             *pFrom = set_bits<N, E>(*pFrom, v, bits);
         }
 
-        template<uint8_t N>
+        template<auto N>
         constexpr uint32_t get_bits(uint32_t from, uint32_t v)
         {
-            return (v >> uint32_t(from)) & ((uint32_t(1) << N) - 1);
+            return (v >> uint32_t(from)) & ((uint32_t(1) << uint8_t(N)) - 1);
         }
 
-        template<auto from, uint8_t N>
+        template<auto from, auto N>
         constexpr uint32_t clear_bits(uint32_t v)
         {
-            constexpr uint32_t kClearMask = ((uint32_t(1) << N) - 1) << uint32_t(from);
+            constexpr uint32_t kClearMask = ((uint32_t(1) << uint8_t(N)) - 1) << uint32_t(from);
             return (v & ~kClearMask);
         }
 
-        template<auto from, uint8_t N, class E>
+        template<auto from, auto N, class E>
         constexpr uint32_t set_bits(uint32_t v, E bits)
         {
             return clear_bits<from, N>(v) | (uint32_t(bits) << uint32_t(from));
@@ -67,10 +67,10 @@ namespace rpi
             *pFrom = set_bits<from, N, E>(*pFrom, bits);
         }
 
-        template<auto from, uint8_t N>
+        template<auto from, auto N>
         constexpr uint32_t get_bits(uint32_t v)
         {
-            return (v >> uint32_t(from)) & ((uint32_t(1) << N) - 1);
+            return (v >> uint32_t(from)) & ((uint32_t(1) << uint8_t(N)) - 1);
         }
 
         template<class T>
