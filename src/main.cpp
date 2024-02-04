47413a1b5dffa8370f39a6097f97a6d64c631fdb
theorlangur <theorlangur@gmail.com>
2024-02-04 16:36:28 +0100

    measuring and outputing time needed for various actions

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,40 @@ extern "C" void kernel_main()
     using I2CPins = rpi::RPiBplus::I2C1_Pins;
     using I2C = rpi::i2c::I2C<RPi, I2CPins>;
 
-    I2C::Init i2cInit;
+    /*I2C::Init i2cInit;
     uint8_t xxx = 0;
     auto wr = I2C::write(&xxx, 1);
     if (wr.has_value())
     {
         //success
-    }
+    }*/
 
+    Timer::TimeTest ttRpiInit;
     RPi::Init rpiInit;
+    ttRpiInit.mark();
+    Timer::TimeTest ttCostructDisplayInit;
     D d;
+    ttCostructDisplayInit.mark();
     //the order here is important
     //1. GPIO init
+    Timer::TimeTest ttDisplayGPIOInit;
     d.init_gpio_pins();
+    ttDisplayGPIOInit.mark();
     //2. SPI init
+    Timer::TimeTest ttSPIInit;
     rpi::spi::SPIInit<RPi, SPI> spiInit(CS);
+    ttSPIInit.mark();
     //3. Display init
+    Timer::TimeTest ttDisplayInit;
     d.init();
+    ttDisplayInit.mark();
 
+    Timer::TimeTest ttFontsInit;
     display::font::init();
+    ttFontsInit.mark();
+    Timer::TimeTest ttIconsInit;
     display::icons::misc::init();
+    ttIconsInit.mark();
     //Timer::delay_ms(10000);
 
     const auto &symTrizub = display::icons::misc::symTrizub;
@@ -49,13 +63,31 @@ extern "C" void kernel_main()
     Renderer r(d);
 
     r.clear();
-    r.render_symbol({0,0}, symTrizub, {5,2}, {7,9});
+    Timer::TimeTest ttPrint;
+    r.print_all({0,0}
+            , "Init:\n",
+            "RPI:",ttRpiInit.measured(),
+            "\nDispCTR:",ttCostructDisplayInit.measured(),
+            "\nDispGPIO:", ttDisplayGPIOInit.measured(),
+            "\nSPI:",ttSPIInit.measured(),
+            "\nDisp:",ttDisplayInit.measured(),
+            "\nFonts:",ttFontsInit.measured(),
+            "\nIcons:",ttIconsInit.measured()
+            );
+    ttPrint.mark();
+    /*r.render_symbol({0,0}, symTrizub, {5,2}, {7,9});
     r.draw_char({10,0}, '0');
     r.render_line({0, D::kDisplayHeight / 2}, {D::kDisplayWidth - 1, D::kDisplayHeight / 2});
     r.render_line({D::kDisplayWidth / 2, 0}, {D::kDisplayWidth / 2, D::kDisplayHeight - 1});
     r.render_line({0, 0}, {D::kDisplayWidth - 1, D::kDisplayHeight - 1});
     r.render_line({0, D::kDisplayHeight - 1}, {D::kDisplayWidth - 1, 0});
+    */
     r.show();
+    Timer::delay_ms(20000);
+    r.clear();
+    r.print_all({0,0} , "Previous printing took:\n" ,ttPrint.measured(), " us");
+    r.show();
+    Timer::delay_ms(20000);
 
     for(uint8_t y = 1; y < 8; ++y)
     {
